ad5ecfdc6acd65c5ae7ecca728dfe213a117be77
Dan Skorupski <boardwalk@ersatsz.com>
2019-03-08 08:54:10 -0600

    Use macro to impl tuples.

--- foundationdb-util/src/tuple/tuple.rs
+++ foundationdb-util/src/tuple/tuple.rs
@@ -2,92 +2,58 @@ use crate::tuple::{expect, Pack, Unpack, UnpackError};
 
 const NESTED_CODE: u8 = 0x05;
 
-impl<T1> Pack for (T1,)
-where
-    T1: Pack,
-{
-    fn pack(&self, out: &mut Vec<u8>, nested: bool) {
-        let (v1,) = self;
-        if nested {
-            out.push(NESTED_CODE);
-        }
-        T1::pack(v1, out, true);
-        if nested {
-            out.push(0x00);
-        }
-    }
-}
-
-impl<T1> Unpack for (T1,)
-where
-    T1: Unpack,
-{
-    fn unpack(inp: &[u8], nested: bool) -> Result<(Self, &[u8]), UnpackError> {
-        let inp = if nested {
-            expect(inp, NESTED_CODE)?
-        } else {
-            inp
-        };
-        let (v1, inp) = T1::unpack(inp, true)?;
-        let inp = if nested {
-            expect(inp, 0x00)?
-        } else {
-            inp
-        };
-        Ok(((v1,), inp))
-    }
-}
-
-impl<T1, T2> Pack for (T1, T2)
-where
-    T1: Pack,
-    T2: Pack,
-{
-    fn pack(&self, out: &mut Vec<u8>, nested: bool) {
-        let (v1, v2) = self;
-        if nested {
-            out.push(NESTED_CODE);
-        }
-        T1::pack(v1, out, true);
-        T2::pack(v2, out, true);
-        if nested {
-            out.push(0x00);
-        }
-    }
-}
-
-impl<T1, T2> Unpack for (T1, T2)
-where
-    T1: Unpack,
-    T2: Unpack,
-{
-    fn unpack(inp: &[u8], nested: bool) -> Result<(Self, &[u8]), UnpackError> {
-        let inp = if nested {
-            expect(inp, NESTED_CODE)?
-        } else {
-            inp
-        };
-        let (v1, inp) = T1::unpack(inp, true)?;
-        let (v2, inp) = T2::unpack(inp, true)?;
-        let inp = if nested {
-            expect(inp, 0x00)?
-        } else {
-            inp
-        };
-        Ok(((v1, v2), inp))
-    }
-}
-
-// TODO: Implement higher arity
-
 pub trait Tuple: Pack + Unpack {}
-impl<T1> Tuple for (T1,)
-where
-    T1: Pack + Unpack,
-{}
 
-impl<T1, T2> Tuple for (T1, T2)
-where
-    T1: Pack + Unpack,
-    T2: Pack + Unpack,
-{}
+macro_rules! impl_tuple {
+    ( $($id:ident : $ty:tt),* ) => {
+        impl< $($ty),* > Pack for ( $($ty,)* )
+        where
+            $($ty: Pack,)*
+        {
+            fn pack(&self, out: &mut Vec<u8>, nested: bool) {
+                let ( $($id,)* ) = self;
+                if nested {
+                    out.push(NESTED_CODE);
+                }
+                $($ty::pack($id, out, true);)*
+                if nested {
+                    out.push(0x00);
+                }
+            }
+        }
+
+        impl< $($ty,)* > Unpack for ( $($ty,)* )
+        where
+            $($ty: Unpack,)*
+        {
+            fn unpack(inp: &[u8], nested: bool) -> Result<(Self, &[u8]), UnpackError> {
+                let inp = if nested {
+                    expect(inp, NESTED_CODE)?
+                } else {
+                    inp
+                };
+                $(let ($id, inp) = $ty::unpack(inp, true)?;)*
+                let inp = if nested {
+                    expect(inp, 0x00)?
+                } else {
+                    inp
+                };
+                Ok((( $($id,)* ), inp))
+            }
+        }
+
+        impl< $($ty,)* > Tuple for ( $($ty,)* )
+        where
+            $($ty: Pack + Unpack,)*
+        {}
+    };
+}
+
+impl_tuple!(v1: T1);
+impl_tuple!(v1: T1, v2: T2);
+impl_tuple!(v1: T1, v2: T2, v3: T3);
+impl_tuple!(v1: T1, v2: T2, v3: T3, v4: T4);
+impl_tuple!(v1: T1, v2: T2, v3: T3, v4: T4, v5: T5);
+impl_tuple!(v1: T1, v2: T2, v3: T3, v4: T4, v5: T5, v6: T6);
+impl_tuple!(v1: T1, v2: T2, v3: T3, v4: T4, v5: T5, v6: T6, v7: T7);
+impl_tuple!(v1: T1, v2: T2, v3: T3, v4: T4, v5: T5, v6: T6, v7: T7, v8: T8);
